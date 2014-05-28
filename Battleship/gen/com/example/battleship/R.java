ff2132795c9d32077c57ec4bd95ff58864b1e4ff
riajones <riajones@ucsc.edu>
2014-05-28 13:52:20 -0700

    I made a bunch of menus. BTW GitHub doesn't accept empty folders so I had to delete the asset folder in the Battleship folder and the drawable-ldip folder int he Battleship->res folder

--- Battleship/gen/com/example/battleship/R.java
+++ Battleship/gen/com/example/battleship/R.java
@@ -1092,10 +1092,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
+        public static final int backBtn=0x7f050041;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050045;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1118,6 +1120,9 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int optionsBackBtn=0x7f050040;
+        public static final int optionsView=0x7f05003f;
+        public static final int otherButton=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1131,11 +1136,15 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int setupStartBtn=0x7f050043;
+        public static final int setupView=0x7f050042;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int singlePlayer_checkBox=0x7f050044;
         public static final int split_action_bar=0x7f05001e;
+        public static final int startBtn=0x7f05003d;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
@@ -1195,12 +1204,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int options=0x7f030019;
+        public static final int setup_game=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int Options=0x7f0a0013;
+        public static final int Setup=0x7f0a0015;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1243,7 +1256,11 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int back=0x7f0a0012;
+        public static final int different=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
+        public static final int singlePlayer=0x7f0a0014;
+        public static final int start=0x7f0a0010;
     }
     public static final class style {
         /** 
--- Battleship/src/com/example/battleship/MainActivity.java
+++ Battleship/src/com/example/battleship/MainActivity.java
@@ -6,43 +6,53 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Display;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	public Graph aGraph;
 	public int sizeX, sizeY;
-
+	public View main;
+	public aView game;
+	public int curView;
+	public boolean singlePlayer;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //This is where we are creating our view
-        aView mainView = new aView(getApplicationContext());
-        setContentView(mainView);
+        singlePlayer = false;
+        game = new aView(getApplicationContext());
+        setContentView(R.layout.activity_main);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         sizeX = size.x;
         sizeY = size.y;
         //Setting up the system that senses 
-        mainView.setOnTouchListener(new View.OnTouchListener(){
+        game.setOnTouchListener(new View.OnTouchListener(){
         	@Override
         	public boolean onTouch(View v, MotionEvent event){
+        		
+        		//Determining Which Square the user has clicked and marking it as clicked
         		int x =(int)(event.getX() / (sizeX / 10));
         		int y = (int)(event.getY() / (sizeY / 10));
-        		String temp = "You pressed " + x + ", " + y;
-        		Toast toast = Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG);
-        		toast.show();
         		aGraph.touch(x,y);
         		return true;
         	}
         });
         //Creating the graph that tracks the back of the screen
         aGraph = new Graph();
+        
+        curView = 0;
+        
     }
 
     @Override
@@ -52,7 +62,22 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    
+    @Override
+    public void onBackPressed() {
+		if(curView == 0 || curView == 1){
+			setContentView(R.layout.activity_main);
+			if(curView == 1)
+				curView = 0;
+		}
+		else if(curView == 2){
+			setContentView(R.layout.activity_main);
+			curView = 0;
+    	}
+		else{
+			setContentView(game);
+			curView = 2;
+		}    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -71,21 +96,66 @@ public class MainActivity extends ActionBarActivity {
     //Additionally this is necessary in order to do work on the canvas (the way that the background and sensor system is
     //currently working)
     public class aView extends View{
-	    	
+	    
+    	LinearLayout layout;
+    	Button attack;
+    	
     	public aView(Context context){
     		super(context);    		
     		this.setBackgroundResource(R.drawable.background1);
+
     	}
 	
     	@Override
     	protected void onDraw(Canvas canvas){
     		super.onDraw(canvas);
-    		//If we want to have extra things drawn on the screen, this is where the code shoudl go
+    		//If we want to have extra things drawn on the screen, this is where the code should go
+    		
     	}
 	
 	}
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ///////////Creating Functions that correspond to various buttons////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
     
+	public void main_start_btn(View v){		
+        setContentView(R.layout.setup_game);
+        curView = 1;
+	}
+	
+	public void setup_start_btn(View v){
+		curView = 2;
+		if(singlePlayer){
+			setContentView(game);
+		}
+		else{
+			setContentView(game);
+		}
+	}
 
+	public void back_btn(View v){
+		if(curView == 0 || curView == 1){
+			setContentView(R.layout.activity_main);
+			if(curView == 1)
+				curView = 0;
+		}
+		else{
+			setContentView(game);
+			curView = 2;
+		}
+	}
+	
+	public void options_single_player(View v){
+		singlePlayer = !singlePlayer;
+	}
+	
+	public void different_btn(View v){
+		setContentView(R.layout.options);
+	}
+	
+	
     ////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////
     //////////This is the segment of code dedicated to storing data on the game state///////
@@ -107,7 +177,7 @@ public class MainActivity extends ActionBarActivity {
     		graph[x][y].state = 1;
     		Context context = getApplicationContext();
     		String temp = "You pressed " + x + ", " + y;
-    		Toast toast = Toast.makeText(context, temp, Toast.LENGTH_LONG);
+    		Toast toast = Toast.makeText(context, temp, Toast.LENGTH_SHORT);
     		toast.show();
     	}
     	
@@ -124,5 +194,4 @@ public class MainActivity extends ActionBarActivity {
     		state = 0;
     	}	
     }
-
 }
